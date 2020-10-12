4c0f31609e9a0f21ce0f63966297a9c734bc86c4
Lars van den Brandt <Lars.vandenbrandt@me.com>
2020-10-12 15:53:05 +0200

    login page added

---
+++ Abracadabra/abracadabra_web_app/src/components/Login.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope, faHome, faLock, faSearch, faTimes, faUser, faUserCircle, faUsers } from '@fortawesome/free-solid-svg-icons';
+
+function Title() {
+  return (
+    <div className="container">
+			<div className="row main">
+				<div className="main-login main-center">
+                    <h2 className="text-center">Login</h2>
+					<form>
+						<div class="form-group input-group">
+							<div class="input-group-prepend">
+		    					<span class="input-group-text"> <FontAwesomeIcon icon={faEnvelope}/></span>
+		 					</div>
+        					<input name="email" class="form-control" placeholder="Email" type="text"/>
+    					</div>
+
+						<div class="form-group input-group">
+							<div class="input-group-prepend">
+		    					<span class="input-group-text"> <FontAwesomeIcon icon={faLock}/></span>
+		 					</div>
+        					<input name="password" class="form-control" placeholder="Password" type="text"/>
+    					</div>
+						<div className="form-group ">
+							<button type="button" className="btn btn-primary btn-lg btn-block login-button">Login</button>
+						</div>
+						<div className="login-register">
+				            <a href="/registerpage">Don't have an account? Create one!</a>
+				         </div>
+					</form>
+				</div>
+			</div>
+		</div>
+  );
+}
+
+export default Title;
--- Abracadabra/abracadabra_web_app/src/components/Register.js
+++ Abracadabra/abracadabra_web_app/src/components/Register.js
@@ -7,63 +7,47 @@ function Title() {
     <div className="container">
 			<div className="row main">
 				<div className="main-login main-center">
-                    <h2>Abra cadabra</h2>
-					<form className="form-horizontal" method="post" action="#">
-						<div className="form-group">
-							<label for="name" className="cols-sm-2 control-label">Your Name</label>
-							<div className="cols-sm-10">
-								<div className="input-group">
-									<span className="input-group-addon"> <FontAwesomeIcon icon={faUser} aria-hidden="true" /></span>
-									<input type="text" className="form-control" name="name" id="name"  placeholder="Enter your Name"/>
-								</div>
-							</div>
-						</div>
+                    <h2 className="text-center">Register</h2>
+					<form>
+						<div class="form-group input-group">
+							<div class="input-group-prepend">
+		    					<span class="input-group-text" style={{width:"45px"}}> <FontAwesomeIcon icon={faUser}/></span>
+		 					</div>
+        					<input name="name" class="form-control" placeholder="Full name" type="text"/>
+    					</div>
 
-						<div className="form-group">
-							<label for="email" className="cols-sm-2 control-label">Your Email</label>
-							<div className="cols-sm-10">
-								<div className="input-group">
-									<span className="input-group-addon"><FontAwesomeIcon icon={faEnvelope} aria-hidden="true" /></span>
-									<input type="text" className="form-control" name="email" id="email"  placeholder="Enter your Email"/>
-								</div>
-							</div>
-						</div>
+						<div class="form-group input-group">
+							<div class="input-group-prepend">
+		    					<span class="input-group-text" style={{width:"45px"}}> <FontAwesomeIcon icon={faEnvelope}/></span>
+		 					</div>
+        					<input name="email" class="form-control" placeholder="Email" type="text"/>
+    					</div>
 
-						<div className="form-group">
-							<label for="username" className="cols-sm-2 control-label">Username</label>
-							<div className="cols-sm-10">
-								<div className="input-group">
-									<span className="input-group-addon"><FontAwesomeIcon icon={faUsers} aria-hidden="true" /></span>
-									<input type="text" className="form-control" name="username" id="username"  placeholder="Enter your Username"/>
-								</div>
-							</div>
-						</div>
+						<div class="form-group input-group">
+							<div class="input-group-prepend">
+		    					<span class="input-group-text" style={{width:"45px"}}> <FontAwesomeIcon icon={faUsers}/></span>
+		 					</div>
+        					<input name="username" class="form-control" placeholder="Username" type="text"/>
+    					</div>
 
-						<div className="form-group">
-							<label for="password" className="cols-sm-2 control-label">Password</label>
-							<div className="cols-sm-10">
-								<div className="input-group">
-									<span className="input-group-addon"><FontAwesomeIcon icon={faLock} aria-hidden="true" /></span>
-									<input type="password" className="form-control" name="password" id="password"  placeholder="Enter your Password"/>
-								</div>
-							</div>
-						</div>
+						<div class="form-group input-group">
+							<div class="input-group-prepend">
+		    					<span class="input-group-text" style={{width:"45px"}}> <FontAwesomeIcon icon={faLock}/></span>
+		 					</div>
+        					<input name="password" class="form-control" placeholder="Password" type="text"/>
+    					</div>
 
-						<div className="form-group">
-							<label for="confirm" className="cols-sm-2 control-label">Confirm Password</label>
-							<div className="cols-sm-10">
-								<div className="input-group">
-									<span className="input-group-addon"><FontAwesomeIcon icon={faLock} aria-hidden="true" /></span>
-									<input type="password" className="form-control" name="confirm" id="confirm"  placeholder="Confirm your Password"/>
-								</div>
-							</div>
-						</div>
-
+						<div class="form-group input-group">
+							<div class="input-group-prepend">
+		    					<span class="input-group-text" style={{width:"45px"}}> <FontAwesomeIcon icon={faLock}/></span>
+		 					</div>
+        					<input name="password" class="form-control" placeholder="Repeat password" type="text"/>
+    					</div>
 						<div className="form-group ">
 							<button type="button" className="btn btn-primary btn-lg btn-block login-button">Register</button>
 						</div>
 						<div className="login-register">
-				            <a href="index.php">Login</a>
+				            <a href="/loginpage">Login</a>
 				         </div>
 					</form>
 				</div>
