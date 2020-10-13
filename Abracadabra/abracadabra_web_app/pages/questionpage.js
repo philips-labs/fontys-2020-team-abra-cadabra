d8016096e3caea07b7fd33895a90054b8d92453a
i403318 <realsnowkid@gmail.com>
2020-10-13 10:22:32 +0300

    Add initial styling for the page

--- Abracadabra/abracadabra_web_app/pages/questionpage.js
+++ Abracadabra/abracadabra_web_app/pages/questionpage.js
@@ -1,13 +1,23 @@
-import QuestionHead from "../src/components/QuestionHead";
-import QuestionAnwser from "../src/components/QuestionAnwser";
 import Navbar from "../src/components/Navbar";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons';
 
 function QuestionPage() {
-    return <div>
-        <QuestionHead />
-        <QuestionAnwser />
-    </div>
-
+    return (
+        <>
+            <Navbar />
+            <div className="row mx-auto questionHead">
+                <h1 className="col-md-8 questionTitle">How do you dice an onion?</h1>
+                <div className="col-md-4 d-flex justify-content-end">
+                    <FontAwesomeIcon className="questionAvatar" icon={faUser} />
+                    <p className="questionUsername">User4321</p>
+                </div>
+                <div className="row questionContent">
+                    <p>I don't understand how I can cut an onion, can someone hlep me?</p>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default QuestionPage
