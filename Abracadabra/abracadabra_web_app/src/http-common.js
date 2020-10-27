fb17f9c067c38902164b9ddcea106d055435de2d
Kevin Bevers <kevin.bevers@hotmail.com>
2020-10-27 14:28:38 +0100

    changed http-common locally, dont check in

--- Abracadabra/abracadabra_web_app/src/http-common.js
+++ Abracadabra/abracadabra_web_app/src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "https://localhost:44343/api",
+  baseURL: "https://10.211.55.3:45455/api",
   headers: {
     "Content-type": "application/json",
   },
