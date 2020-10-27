d8ce2389af4432b9f7340374774be33790a12414
VoidBGS <kristian_robert@abv.bg>
2020-10-27 12:39:01 +0200

    Added QuestionViewModel that returns a username, Added Mapper Class

---
+++ Abracadabra/AbracadabraAPI/AbracadabraAPI/Mappers/Mapper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbracadabraAPI.Models;
+using AbracadabraAPI.Data;
+using AbracadabraAPI.ViewModels;
+using Microsoft.AspNetCore.Identity;
+
+namespace AbracadabraAPI.Mappers
+{
+    public static class Mapper
+    {
+        public static QuestionViewModel QuestionToViewModel(Question question, IdentityUser user, AbracadabraContext _context) =>
+        new QuestionViewModel
+        {
+            ID = question.ID,
+            Title = question.Title,
+            Description = question.Description,
+            Category = question.Category,
+            UserName = user.UserName,
+            DateTimeCreated = question.DateTimeCreated,
+            Answers = _context.Answers.Where(x => x.QuestionID == question.ID).ToList()
+        };
+    }
+}
