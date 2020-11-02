f3e13677ca201bb1385bbcbf3c6c93de608d5f9d
Lars van den Brandt <Lars.vandenbrandt@me.com>
2020-11-02 09:44:58 +0100

    Front end testing system

---
+++ Abracadabra/abracadabra_web_app/__tests__/login.test.js
@@ -0,0 +1,9 @@
+const {
+  interopDefault,
+} = require("next/dist/next-server/server/load-components");
+
+describe("Test subjects", () => {
+  it("Should return 404 if subject not found", () => {});
+
+  it("Should return page if subject is found", () => {});
+});
