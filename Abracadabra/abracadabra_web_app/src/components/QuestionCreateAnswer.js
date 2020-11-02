6bb6ea182e8883d39e896faad11b719385f24d88
TimonC <58507497+TimonC@users.noreply.github.com>
2020-11-02 13:51:22 +0100

    Added await + removed prevent default

--- Abracadabra/abracadabra_web_app/src/components/QuestionCreateAnswer.js
+++ Abracadabra/abracadabra_web_app/src/components/QuestionCreateAnswer.js
@@ -25,7 +25,7 @@ function QuestionCreateAnwser(questionSendId) {
         setAnswer({ answercontent: '', questionid: questionSendId.QID })
     }, [questionSendId])
 
-    const handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
 
         const form = event.currentTarget;
         if (form.checkValidity() === false) {
@@ -33,8 +33,7 @@ function QuestionCreateAnwser(questionSendId) {
             event.stopPropagation();
         }
         else {
-            event.preventDefault();
-            QuestionService.QuestionAnswer(Answer).then((res) => {
+            await QuestionService.QuestionAnswer(Answer).then((res) => {
                 console.log(res);
                 console.log(res.data);
             })
