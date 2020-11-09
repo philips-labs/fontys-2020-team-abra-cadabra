57e08b1169f80fbd44108cd4565bd06a10133dc1
Snechar <Snechboii@gmail.com>
2020-11-09 12:11:58 +0100

    Added Question Filter and Mappers

---
+++ Abracadabra/AbracadabraAPI/AbracadabraAPI/ViewModels/SubjectWithQuestionsViewModel.cs
@@ -0,0 +1,18 @@
+﻿using AbracadabraAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AbracadabraAPI.ViewModels
+{
+    public class SubjectWithQuestionsViewModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string SubjectName { get; set; }
+        public List<QuestionWithNoAnswersViewModel> Questions { get; set; }
+
+    }
+}
