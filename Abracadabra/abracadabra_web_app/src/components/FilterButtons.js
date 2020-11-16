eeb99ebe9a7effea5f4f1881cf1116791d67bda2
i403318 <realsnowkid@gmail.com>
2020-11-16 15:30:40 +0200

    Add href links to FilterButtons

--- Abracadabra/abracadabra_web_app/src/components/FilterButtons.js
+++ Abracadabra/abracadabra_web_app/src/components/FilterButtons.js
@@ -3,17 +3,17 @@ import { Button, Row, Col } from 'react-bootstrap';
 import { faChartLine, faFireAlt, faCertificate, faComment, faCommentSlash, faUserGraduate } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-function FilterButtons() {
+function FilterButtons({ subjectTitle }) {
     return (
         <div class="rounded container">
             <Row>
                 <Col md="7">
-                    <Button variant="secondary" className="btnSmall"><FontAwesomeIcon icon={faCertificate} /> New</Button>
-                    <Button variant="secondary" className="btnSmall"><FontAwesomeIcon icon={faFireAlt} /> Hot</Button>
-                    <Button variant="secondary" className="btnSmall"><FontAwesomeIcon icon={faChartLine} /> Top</Button>
+                    <Button variant="secondary" className="btnSmall" href={'/subject/' + subjectTitle + '/new'}><FontAwesomeIcon icon={faCertificate} /> New</Button>
+                    <Button variant="secondary" className="btnSmall" href={'/subject/' + subjectTitle}><FontAwesomeIcon icon={faFireAlt} /> Hot</Button>
+                    <Button variant="secondary" className="btnSmall" href={'/subject/' + subjectTitle + '/top'}><FontAwesomeIcon icon={faChartLine} /> Top</Button>
                 </Col>
                 <Col md="5">
-                    <Button variant="secondary" className="btnBig"><FontAwesomeIcon icon={faCommentSlash} /> Unanswered</Button>
+                    <Button variant="secondary" className="btnBig" href={'/subject/' + subjectTitle + '/unanswered'}><FontAwesomeIcon icon={faCommentSlash} /> Unanswered</Button>
                     <Button variant="secondary" className="btnBig"><FontAwesomeIcon icon={faComment} /> Answered</Button>
                     <Button variant="secondary" className="btnBig"><FontAwesomeIcon icon={faUserGraduate} /> Expert Answered</Button>
                 </Col>
