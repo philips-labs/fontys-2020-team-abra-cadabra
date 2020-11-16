0e835375f78527fca3b23d329cc31fa2c7d88848
TimonC <58507497+TimonC@users.noreply.github.com>
2020-11-16 14:34:19 +0100

    Added css for login

--- Abracadabra/abracadabra_web_app/src/components/Login.js
+++ Abracadabra/abracadabra_web_app/src/components/Login.js
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faLock } from "@fortawesome/free-solid-svg-icons";
 import AccountService from "../services/AccountService";
 import Router from "next/router";
+import { Row } from 'react-bootstrap';
 
 const Login = () => {
   const [login, setLogin] = useState({ email: "", password: "" });
@@ -25,62 +26,75 @@ const Login = () => {
       });
   };
   return (
-    <div className="main-login main-center">
-      <h2 className="text-center">Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div className="form-group input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              {" "}
-              <FontAwesomeIcon icon={faEnvelope} />
-            </span>
-          </div>
-          <input
-            onChange={handleChange}
-            name="email"
-            className="form-control"
-            placeholder="Email"
-            type="email"
-            required
-          />
-        </div>
+   
+    <div className="container mt-4">
+    
+Top
 
-        <div className="form-group input-group">
-          <div className="input-group-prepend">
-            <span className="input-group-text">
-              {" "}
-              <FontAwesomeIcon icon={faLock} />
-            </span>
-          </div>
-          <input
-            onChange={handleChange}
-            name="password"
-            className="form-control"
-            placeholder="Password"
-            type="password"
-            required
-          />
-        </div>
-        <div className="form-group ">
-          <button
-            type="submit"
-            className="btn btn-primary btn-lg btn-block login-button"
-          >
-            Login
-          </button>
-        </div>
-        <div className="login-register">
-          <a href="/registerpage">Don't have an account? Create one!</a>
-        </div>
-        <div
-          className="text-danger"
-          role="alert"
-          style={{ textAlign: "center" }}
-        >
-          {message}
-        </div>
-      </form>
-    </div>
+</div>
+    
+
+
+
+
+
+
+    // <div className="main-login main-center">
+    //   <h2 className="text-center">Login</h2>
+    //   <form onSubmit={handleSubmit}>
+    //     <div className="form-group input-group">
+    //       <div className="input-group-prepend">
+    //         <span className="input-group-text">
+    //           {" "}
+    //           <FontAwesomeIcon icon={faEnvelope} />
+    //         </span>
+    //       </div>
+    //       <input
+    //         onChange={handleChange}
+    //         name="email"
+    //         className="form-control"
+    //         placeholder="Email"
+    //         type="email"
+    //         required
+    //       />
+    //     </div>
+
+    //     <div className="form-group input-group">
+    //       <div className="input-group-prepend">
+    //         <span className="input-group-text">
+    //           {" "}
+    //           <FontAwesomeIcon icon={faLock} />
+    //         </span>
+    //       </div>
+    //       <input
+    //         onChange={handleChange}
+    //         name="password"
+    //         className="form-control"
+    //         placeholder="Password"
+    //         type="password"
+    //         required
+    //       />
+    //     </div>
+    //     <div className="form-group ">
+    //       <button
+    //         type="submit"
+    //         className="btn btn-primary btn-lg btn-block login-button"
+    //       >
+    //         Login
+    //       </button>
+    //     </div>
+    //     <div className="login-register">
+    //       <a href="/registerpage">Don't have an account? Create one!</a>
+    //     </div>
+    //     <div
+    //       className="text-danger"
+    //       role="alert"
+    //       style={{ textAlign: "center" }}
+    //     >
+    //       {message}
+    //     </div>
+    //   </form>
+    // </div>
   );
 };
 
