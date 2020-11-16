039cd03f6d677257ec54f5bff71125e9693e4fde
Lars van den Brandt <Lars.vandenbrandt@me.com>
2020-11-16 16:09:23 +0100

    2 more mergeconflicts

--- Abracadabra/abracadabra_web_app/src/components/FilterButtons.js
+++ Abracadabra/abracadabra_web_app/src/components/FilterButtons.js
@@ -9,55 +9,50 @@ import {
   faUserGraduate,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-
-<<<<<<< HEAD
-function FilterButtons() {
+function FilterButtons({ subjectTitle }) {
   return (
-    <div className="rounded container">
+    <div class="rounded container">
       <Row>
         <Col md="7">
-          <Button variant="primary">
+          <Button
+            variant="secondary"
+            className="btnSmall"
+            href={"/subject/" + subjectTitle + "/new"}
+          >
             <FontAwesomeIcon icon={faCertificate} /> New
           </Button>
-          <Button variant="primary">
+          <Button
+            variant="secondary"
+            className="btnSmall"
+            href={"/subject/" + subjectTitle}
+          >
             <FontAwesomeIcon icon={faFireAlt} /> Hot
           </Button>
-          <Button variant="primary">
+          <Button
+            variant="secondary"
+            className="btnSmall"
+            href={"/subject/" + subjectTitle + "/top"}
+          >
             <FontAwesomeIcon icon={faChartLine} /> Top
           </Button>
         </Col>
         <Col md="5">
-          <Button variant="secondary">
+          <Button
+            variant="secondary"
+            className="btnBig"
+            href={"/subject/" + subjectTitle + "/unanswered"}
+          >
             <FontAwesomeIcon icon={faCommentSlash} /> Unanswered
           </Button>
-          <Button variant="secondary">
+          <Button variant="secondary" className="btnBig">
             <FontAwesomeIcon icon={faComment} /> Answered
           </Button>
-          <Button variant="secondary">
+          <Button variant="secondary" className="btnBig">
             <FontAwesomeIcon icon={faUserGraduate} /> Expert Answered
           </Button>
         </Col>
       </Row>
     </div>
   );
-=======
-function FilterButtons({ subjectTitle }) {
-    return (
-        <div class="rounded container">
-            <Row>
-                <Col md="7">
-                    <Button variant="secondary" className="btnSmall" href={'/subject/' + subjectTitle + '/new'}><FontAwesomeIcon icon={faCertificate} /> New</Button>
-                    <Button variant="secondary" className="btnSmall" href={'/subject/' + subjectTitle}><FontAwesomeIcon icon={faFireAlt} /> Hot</Button>
-                    <Button variant="secondary" className="btnSmall" href={'/subject/' + subjectTitle + '/top'}><FontAwesomeIcon icon={faChartLine} /> Top</Button>
-                </Col>
-                <Col md="5">
-                    <Button variant="secondary" className="btnBig" href={'/subject/' + subjectTitle + '/unanswered'}><FontAwesomeIcon icon={faCommentSlash} /> Unanswered</Button>
-                    <Button variant="secondary" className="btnBig"><FontAwesomeIcon icon={faComment} /> Answered</Button>
-                    <Button variant="secondary" className="btnBig"><FontAwesomeIcon icon={faUserGraduate} /> Expert Answered</Button>
-                </Col>
-            </Row>
-        </div>
-    )
->>>>>>> 598ad2e22f8150ceb8e19c6a8b8d4ac6d40ee6a5
 }
 export default FilterButtons;
--- Abracadabra/abracadabra_web_app/src/components/QuestionBody.js
+++ Abracadabra/abracadabra_web_app/src/components/QuestionBody.js
@@ -20,14 +20,13 @@ function Title({ question, subject }) {
     date = date.toUTCString().split(", ");
     date = date[1].slice(0, 17);
 
-    return (date);
+    return date;
   }
 
   function NrOfAnswers(numberOfAnswers) {
     if (numberOfAnswers == 1) {
       return numberOfAnswers + " person responded";
-    }
-    else {
+    } else {
       return numberOfAnswers + " people responded";
     }
   }
@@ -40,38 +39,13 @@ function Title({ question, subject }) {
             <div className="BodyQuestion-CardBody">
               <div className="row">
                 <div className="col-sm-10">
-<<<<<<< HEAD
                   <a
                     className="BodyQuestionText"
-                    href={subject + "/question/" + question.id}
-=======
-                  <a className="BodyQuestionText" href={'/subject/' + subject + '/question/' + question.id}
->>>>>>> 598ad2e22f8150ceb8e19c6a8b8d4ac6d40ee6a5
+                    href={"/subject/" + subject + "/question/" + question.id}
                   >
                     <h4> {question.title} </h4>
                   </a>
                 </div>
-<<<<<<< HEAD
-                <div className="col-sm-2">25 people responded</div>
-              </div>
-            </div>
-            <div className="BodyQuestion-hastag">
-              <div className="row">
-                <div className="col-sm-9">
-                  <span className="badge badge-secondary p-1 mr-1">
-                    #Cutting
-                  </span>
-                  <span className="badge badge-secondary p-1 mr-1">
-                    #Vegetables
-                  </span>
-                  <span className="badge badge-secondary p-1 mr-1">
-                    #mise-en-place
-                  </span>
-                </div>
-                <div className="col-sm-3">
-                  <p>Posted on: {question.dateTimeCreated}</p>
-                </div>
-=======
                 <div className="col-sm-2">
                   {NrOfAnswers(question.numberOfAnswers)}
                 </div>
@@ -82,16 +56,13 @@ function Title({ question, subject }) {
             <div className="row">
               <div className="col-sm-9">
                 <span className="badge badge-info p-1 mr-1">#Cutting</span>
-                <span className="badge badge-info p-1 mr-1">
-                  #Vegetables
-                </span>
+                <span className="badge badge-info p-1 mr-1">#Vegetables</span>
                 <span className="badge badge-info p-1 mr-1">
                   #mise-en-place
                 </span>
               </div>
               <div className="col-sm-3">
                 <p>Posted on: {HumanDateTime(question.dateTimeCreated)}</p>
->>>>>>> 598ad2e22f8150ceb8e19c6a8b8d4ac6d40ee6a5
               </div>
             </div>
           </div>
