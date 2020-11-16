17d5c326d907d46421090d9a04ec2800f9357f27
VoidBGS <kristian_robert@abv.bg>
2020-11-16 16:15:20 +0200

    Added UserController, UserViewModel, User Seeding, Register User Roles

---
+++ Abracadabra/AbracadabraAPI/AbracadabraAPI/Controllers/UsersController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AbracadabraAPI.Authentication;
+using Microsoft.AspNetCore.Identity;
+using AbracadabraAPI.ViewModels;
+using AbracadabraAPI.Mappers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using AbracadabraAPI.Data;
+using System.Security.Cryptography.X509Certificates;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AbracadabraAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly RoleManager<IdentityRole> roleManager;
+        private readonly AbracadabraContext _context;
+
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager, AbracadabraContext context)
+        {
+            this.userManager = userManager;
+            this.roleManager = roleManager;
+            _context = context;
+        }
+
+        //GET: api/Users/[?pageSize=5&pageIndex=3]
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<IEnumerable<UserViewModel>>> GetUsers([FromQuery] int pageSize = 10, [FromQuery] int pageIndex = 0)
+        {
+            var users1 = await userManager.Users.ToListAsync();
+            var users = await userManager.Users.Skip(pageSize * pageIndex).Take(pageSize).ToListAsync();
+            List<UserViewModel> viewModels = new List<UserViewModel>();
+
+            foreach(ApplicationUser user in users)
+            {
+                var roles = await userManager.GetRolesAsync(user);
+                viewModels.Add(Mapper.UserToViewModel(user, roles[0]));
+            }
+
+            return viewModels;
+        }
+
+        // GET api/Users/5
+        [HttpGet("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<UserViewModel>> GetUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await userManager.GetRolesAsync(user);
+
+            return Mapper.UserToViewModel(user, roles[0]);
+        }
+
+
+        //WARNING: PUT MAY NOT BE 'ETHICAL'
+
+        // PUT: api/Questions/5
+        //[HttpPut("{id}")]
+        //[Authorize(Roles = "Admin")]
+        //public async Task<IActionResult> PutQuestion(string id, ApplicationUser passedUser)
+        //{
+        //    var user = await userManager.FindByIdAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    user.UserName = passedUser.UserName;
+        //    user.Email = passedUser.Email;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException ex)
+        //    {
+        //        throw ex;
+        //    }
+
+        //    return NoContent();
+        //}
+
+        // DELETE api/Users/5
+        [HttpDelete("{id}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<UserViewModel>> DeleteUser(string id)
+        {
+            var user = await userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            var roles = await userManager.GetRolesAsync(user);
+
+            _context.Users.Remove(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(Mapper.UserToViewModel(user, roles[0]));
+        }
+    }
+}
---
+++ Abracadabra/AbracadabraAPI/AbracadabraAPI/Data/MyIdentityDataInitializer.cs
@@ -0,0 +1,113 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AbracadabraAPI.Authentication;
+
+namespace AbracadabraAPI.Data
+{
+    public class MyIdentityDataInitializer
+    {
+        public static void SeedData(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            SeedRoles(roleManager);
+            SeedUsers(userManager);
+        }
+
+        public static void SeedUsers(UserManager<ApplicationUser> userManager)
+        {
+            var hasher = new PasswordHasher<ApplicationUser>();
+            if (userManager.FindByNameAsync("test0").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "test0";
+                user.NormalizedUserName = "TEST0";
+                user.Email = "test0@gmail.com";
+                user.NormalizedEmail = "test0@gmail.com".ToUpper();
+                user.EmailConfirmed = false;
+                user.LastLoggedIn = DateTime.Now;
+                user.DateTimeCreated = DateTime.Now;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                IdentityResult result = userManager.CreateAsync
+                (user, "Password@0").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "User").Wait();
+                }
+            }
+
+
+            if (userManager.FindByNameAsync("test1").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "test1";
+                user.NormalizedUserName = "TEST1";
+                user.Email = "test1@gmail.com";
+                user.NormalizedEmail = "test1@gmail.com".ToUpper();
+                user.EmailConfirmed = false;
+                user.LastLoggedIn = DateTime.Now;
+                user.DateTimeCreated = DateTime.Now;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                IdentityResult result = userManager.CreateAsync
+                (user, "Password@1").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user,"Admin").Wait();
+                }
+            }
+
+            if (userManager.FindByNameAsync("test2").Result == null)
+            {
+                ApplicationUser user = new ApplicationUser();
+                user.UserName = "test2";
+                user.NormalizedUserName = "TEST2";
+                user.Email = "test2@gmail.com";
+                user.NormalizedEmail = "test2@gmail.com".ToUpper();
+                user.EmailConfirmed = false;
+                user.LastLoggedIn = DateTime.Now;
+                user.DateTimeCreated = DateTime.Now;
+                user.SecurityStamp = Guid.NewGuid().ToString();
+
+                IdentityResult result = userManager.CreateAsync
+                (user, "Password@2").Result;
+
+                if (result.Succeeded)
+                {
+                    userManager.AddToRoleAsync(user, "Expert").Wait();
+                }
+            }
+        }
+
+        public static void SeedRoles (RoleManager<IdentityRole> roleManager)
+        {
+            if (!roleManager.RoleExistsAsync("User").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "User";
+                IdentityResult roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Expert").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Expert";
+                IdentityResult roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+
+            if (!roleManager.RoleExistsAsync("Admin").Result)
+            {
+                IdentityRole role = new IdentityRole();
+                role.Name = "Admin";
+                IdentityResult roleResult = roleManager.
+                CreateAsync(role).Result;
+            }
+        }
+    }
+}
