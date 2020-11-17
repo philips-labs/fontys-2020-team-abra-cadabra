a32f34fa4dd47ac79496d51949a113c17a32ca54
i403318 <realsnowkid@gmail.com>
2020-11-17 11:49:08 +0200

    Started making the question page

--- Abracadabra/abracadabra_web_app/src/components/SubjectQuestion.js
+++ Abracadabra/abracadabra_web_app/src/components/SubjectQuestion.js
@@ -1,36 +1,31 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
-export default function Question({question}) {
+export default function Question({ question }) {
 
     return (
         <>
-            <div className="mx-auto questionHead">
-            <div className="answerHead mx-auto">
+            <div className="questionHead mx-auto">
                 <h1>Q</h1>
                 <Row>
-                 <Col md={10} className="mx-auto">                 
-                   <Row>
-                     <Col md={9}>
-                     <h3>{question.title}</h3>
-                     </Col>
-                     <Col md={3} className="d-flex justify-content-end">
-                        <img className="questionAvatar" src="https://www.teamphenomenalhope.org/wp-content/uploads/2017/03/avatar-520x520.png"></img>
-                        <p className="questionUsername">{question.userName}</p>
-                     </Col>
-                  </Row>
-                  <Row className="">
-                          <Col>
-                          <p>{question.description}</p>
-                          </Col>
-                   
+                    <Col md={10} className="mx-auto">
+                        <Row>
+                            <Col md={9}>
+                                <h3>{question.title}</h3>
+                            </Col>
+                            <Col md={3} className="d-flex justify-content-end">
+                                <img className="questionPageAvatar" src="https://www.teamphenomenalhope.org/wp-content/uploads/2017/03/avatar-520x520.png"></img>
+                                <p className="questionUsername">{question.userName}</p>
+                            </Col>
+                        </Row>
+                        <Row className="">
+                            <Col>
+                                <p>{question.description}</p>
+                            </Col>
+
+                        </Row>
+                    </Col>
                 </Row>
-                 </Col>
-                </Row>
-                </div>
-                <div className="row justify-content-end">
-                    <button className="questionButton">Submit Own Answer</button>
-                </div>
             </div>
         </>
     );
--- Abracadabra/abracadabra_web_app/src/components/SubjectQuestionAnswer.js
+++ Abracadabra/abracadabra_web_app/src/components/SubjectQuestionAnswer.js
@@ -1,16 +1,41 @@
-import {Container, Row, Col} from 'react-bootstrap';
+import { Card, Row, Col } from 'react-bootstrap';
 
 
-export default function Answer({answer}) {
+export default function Answer({ answer }) {
+
+    function HumanDateTime(dateAndTime) {
+        var date = new Date(dateAndTime + "Z");
+        date = date.toUTCString().split(", ");
+        date = date[1].slice(0, 17);
+
+        return date;
+    }
 
     return (
         <>
             <Row className="border-bottom mb-3">
-                <Col md={10} className="mx-auto">
+                <Col md={11} className="mx-auto">
+
+                    <Card className="answerBody">
+                        <Card.Body>
+                            <Card.Text>
+                                With supporting text below as a natural lead-in to additional content.
+                            </Card.Text>
+                        </Card.Body>
+                        <Card.Footer className="answerFooter">
+                            <div className="d-flex">
+                                <img className="questionPageAvatar" src="https://www.teamphenomenalhope.org/wp-content/uploads/2017/03/avatar-520x520.png"></img>
+                                <p className="answerUsername">{answer.userName}</p>
+                            </div>
+                            <p className="answerPosted">Posted on: {HumanDateTime(answer.dateTimeCreated)}</p>
+                        </Card.Footer>
+                    </Card>
+
+
                     {/* Answer header */}
-                    <Row>
+                    {/* <Row>
                         <Col md={1}>
-                            <img className="questionAvatar" src="https://www.teamphenomenalhope.org/wp-content/uploads/2017/03/avatar-520x520.png"></img>
+                            <img className="questionPageAvatar" src="https://www.teamphenomenalhope.org/wp-content/uploads/2017/03/avatar-520x520.png"></img>
                         </Col>
                         <Col md={7}>
                             <h6 className="font-weight-bold">{answer.userName}</h6>
@@ -19,14 +44,14 @@ export default function Answer({answer}) {
                         <Col md={4}>
                             <h6 className="font-weight-bold helpedText">Has Helped: <span>100</span> People</h6>
                         </Col>
-                    </Row>
+                    </Row> */}
                     {/* Answer content */}
-                    <Row>
+                    {/* <Row>
                         <Col md={1}></Col>
                         <Col>
                             <p className="answerContentText">{answer.answerContent}</p>
                         </Col>
-                    </Row>
+                    </Row> */}
                 </Col>
             </Row>
         </>
