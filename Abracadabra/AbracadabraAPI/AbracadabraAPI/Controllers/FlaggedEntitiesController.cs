b5f646accf15d5e97b70f0eed389f9889e911ad9
dennycox <denny.cox@chello.nl>
2020-11-30 14:37:25 +0100

    Renamed controller and added check to know if already flagged

--- Abracadabra/AbracadabraAPI/AbracadabraAPI/Controllers/FlaggedEntitiesController.cs
+++ Abracadabra/AbracadabraAPI/AbracadabraAPI/Controllers/FlaggedEntitiesController.cs
@@ -8,20 +8,21 @@ using AbracadabraAPI.Models;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace AbracadabraAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class FlagsController : ControllerBase
+    public class FlaggedEntitiesController : ControllerBase
     {
         private readonly AbracadabraContext _context;
-        private readonly UserManager<ApplicationUser> userManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-        public FlagsController(AbracadabraContext context, UserManager<ApplicationUser> userManager)
+        public FlaggedEntitiesController(AbracadabraContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
-            this.userManager = userManager;
+            this._userManager = userManager;
         }
 
         // POST: api/FlaggedQuestions
@@ -29,11 +30,17 @@ namespace AbracadabraAPI.Controllers
         [Authorize]
         public async Task<ActionResult> FlagQuestion(int questionId)
         {
-            var user = await userManager.FindByNameAsync(User.Identity.Name);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
 
             if (user == null)
             {
                 return Unauthorized();
+            }
+
+            var flag = await _context.FlaggedQuestions.Where(x => x.UserId == user.Id && x.QuestionId == questionId).FirstOrDefaultAsync();
+            if (flag != null)
+            {
+                return BadRequest("Question has already been flagged by the user.");
             }
 
             var flaggedQuestion = new FlaggedQuestion
@@ -53,11 +60,17 @@ namespace AbracadabraAPI.Controllers
         [Authorize]
         public async Task<ActionResult> FlagAnswer(int answerId)
         {
-            var user = await userManager.FindByNameAsync(User.Identity.Name);
+            var user = await _userManager.FindByNameAsync(User.Identity.Name);
 
             if (user == null)
             {
                 return Unauthorized();
+            }
+
+            var flag = await _context.FlaggedAnswers.Where(x => x.UserId == user.Id && x.AnswerId == answerId).FirstOrDefaultAsync();
+            if (flag != null)
+            {
+                return BadRequest("Answer has already been flagged by the user.");
             }
 
             var flaggedAnswer = new FlaggedAnswer
