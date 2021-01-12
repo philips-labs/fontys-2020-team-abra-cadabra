7658848603b6a9ce9f74b18dd49c5f1559f1d098
TimonC <58507497+TimonC@users.noreply.github.com>
2021-01-12 10:40:48 +0100

    Added error handeling

--- Abracadabra/abracadabra_web_app/__tests__/filterbuttons.test.js
+++ Abracadabra/abracadabra_web_app/__tests__/filterbuttons.test.js
@@ -8,7 +8,7 @@ import {
     toBeInTheDocument,
   } from "@testing-library/react";
   import { MemoryRouter } from "react-router-dom";
-  const QuestionPageNew = require("../pages/subject/[subject]/expert");
+  const QuestionPageNew = require("../pages/subject/[subject]/index");
   const QuestionPageExpertAnswered = require("../pages/subject/[subject]/expert");
   const QuestionPageTrending = require("../pages/subject/[subject]/trending");
   const QuestionPageUnanswered = require("../pages/subject/[subject]/unanswered");
@@ -76,7 +76,7 @@ import {
       );
      
       const responded = screen.getAllByTestId("question-label-title")[0]
-      expect(responded.textContent).toBe(' Boil water ')
+      expect(responded.textContent).toBe(' How to make spaghetti? ')
     });
     it("Unanswered button returns question with 0 people responded", async () => {
       const params = { subject: "Cooking" };
@@ -109,6 +109,76 @@ import {
       const respondedTitle = screen.getAllByTestId("question-label-title")[0]
       expect(responded.textContent).toBe("2 people responded")
       expect(respondedTitle.textContent).toBe(" Boil water ")
+    });
+    it("Error handeling new", async () => {
+    
+      const params = { subject: "Cooking2" };
+  
+      var response = await QuestionPageNew.getServerSideProps({ params });
+      render(
+        <QuestionPageRenderNew
+          subjectName={response.props.subjectName}
+          response={response.props.response}
+        />
+      );
+      const responded = screen.getByText("This page could not be found.");
+      expect(responded).toBeInTheDocument();
+    });
+    it("Error handeling Trending", async () => {
+    
+      const params = { subject: "Cooking2" };
+  
+      var response = await QuestionPageTrending.getServerSideProps({ params });
+      render(
+        <QuestionPageRenderTrending
+          subjectName={response.props.subjectName}
+          response={response.props.response}
+        />
+      );
+      const responded = screen.getByText("This page could not be found.");
+      expect(responded).toBeInTheDocument();
+    });
+    it("Error handeling Answered", async () => {
+    
+      const params = { subject: "Cooking2" };
+  
+      var response = await QuestionPageAnswered.getServerSideProps({ params });
+      render(
+        <QuestionPageRenderAnswered
+          subjectName={response.props.subjectName}
+          response={response.props.response}
+        />
+      );
+      const responded = screen.getByText("This page could not be found.");
+      expect(responded).toBeInTheDocument();
+    });
+    it("Error handeling Unanswered", async () => {
+    
+      const params = { subject: "Cooking2" };
+  
+      var response = await QuestionPageUnanswered.getServerSideProps({ params });
+      render(
+        <QuestionPageRenderUnanswered
+          subjectName={response.props.subjectName}
+          response={response.props.response}
+        />
+      );
+      const responded = screen.getByText("This page could not be found.");
+      expect(responded).toBeInTheDocument();
+    });
+    it("Error handeling Expert", async () => {
+    
+      const params = { subject: "Cooking2" };
+  
+      var response = await QuestionPageExpertAnswered.getServerSideProps({ params });
+      render(
+        <QuestionPageRenderExpertAnswered
+          subjectName={response.props.subjectName}
+          response={response.props.response}
+        />
+      );
+      const responded = screen.getByText("This page could not be found.");
+      expect(responded).toBeInTheDocument();
     });
    });
   
