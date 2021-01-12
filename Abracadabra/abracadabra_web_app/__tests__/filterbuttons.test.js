c5d5ebebf1e5d3c5788077c7c97f5f3def1d4788
TimonC <58507497+TimonC@users.noreply.github.com>
2021-01-12 10:17:09 +0100

    Fixed filterbuttons test

--- Abracadabra/abracadabra_web_app/__tests__/filterbuttons.test.js
+++ Abracadabra/abracadabra_web_app/__tests__/filterbuttons.test.js
@@ -8,97 +8,107 @@ import {
     toBeInTheDocument,
   } from "@testing-library/react";
   import { MemoryRouter } from "react-router-dom";
-  const QuestionPage = require("../pages/subject/[subject]/index");
-  import QuestionPageRender from "../pages/subject/[subject]/index";
+  const QuestionPageNew = require("../pages/subject/[subject]/expert");
+  const QuestionPageExpertAnswered = require("../pages/subject/[subject]/expert");
+  const QuestionPageTrending = require("../pages/subject/[subject]/trending");
+  const QuestionPageUnanswered = require("../pages/subject/[subject]/unanswered");
+  const QuestionPageAnswered = require("../pages/subject/[subject]/answered");
+  import QuestionPageRenderNew from "../pages/subject/[subject]/index";
+  import QuestionPageRenderTrending from "../pages/subject/[subject]/trending";
+  import QuestionPageRenderExpertAnswered from "../pages/subject/[subject]/expert";
+  import QuestionPageRenderUnanswered from "../pages/subject/[subject]/unanswered";
+  import QuestionPageRenderAnswered from "../pages/subject/[subject]/answered";
   import "@testing-library/jest-dom/extend-expect";
+  
   
   jest.mock("next/router", () => ({
     useRouter() {
       return {
-        route: "/Subject/Cooking",
-        pathname: "/Subject/Cooking",
+        route: "/Subject/Cooking/",
+        pathname: "/Subject/Cooking/",
         query: "",
-        asPath: "Subject/Cooking",
+        asPath: "Subject/Cooking/",
       };
     },
   }));
   
-  
   describe("Filter buttons", () => {
     it("Expert Answered Button returns expert answered question", async () => {
+    
+
       const params = { subject: "Cooking" };
   
-      var response = await QuestionPage.getServerSideProps({ params });
+      var response = await QuestionPageExpertAnswered.getServerSideProps({ params });
       render(
-        <QuestionPageRender
+        <QuestionPageRenderExpertAnswered
           subjectName={response.props.subjectName}
           response={response.props.response}
         />
       );
-      const newButton = await screen.findByTestId("question-button-expert");
-      fireEvent.click(newButton)
-      const responded = await screen.getAllByTestId("question-label-title")[0];
+      const responded = screen.getByTestId("question-label-title");
+      console.log(responded)
+      expect(responded.textContent).toBe(" How to make spaghetti? ");
+    });
+    it("New Button returns newest question", async () => {
+    
+      const params = { subject: "Cooking" };
+  
+      var response = await QuestionPageNew.getServerSideProps({ params });
+      render(
+        <QuestionPageRenderNew
+          subjectName={response.props.subjectName}
+          response={response.props.response}
+        />
+      );
+      const responded = screen.getAllByTestId("question-label-title")[0];
       console.log(responded)
       expect(responded.textContent).toBe(" How to make spaghetti? ");
     });
     it("Hot button returns Boil Water as hottest question", async () => {
       const params = { subject: "Cooking" };
   
-      var response = await QuestionPage.getServerSideProps({ params });
+      var response = await QuestionPageTrending.getServerSideProps({ params });
       render(
-        <QuestionPageRender
+        <QuestionPageRenderTrending
           subjectName={response.props.subjectName}
           response={response.props.response}
         />
       );
-      const newButton = await screen.findByTestId("question-button-trending");
-      await act(async () => {
-      fireEvent.click(newButton)
-      });
+     
       const responded = screen.getAllByTestId("question-label-title")[0]
-      console.log(responded)
-      expect(responded.textContent).toBe("Boil Water")
+      expect(responded.textContent).toBe(' Boil water ')
     });
     it("Unanswered button returns question with 0 people responded", async () => {
       const params = { subject: "Cooking" };
   
-      var response = await QuestionPage.getServerSideProps({ params });
+      var response = await QuestionPageUnanswered.getServerSideProps({ params });
       render(
-        <QuestionPageRender
+        <QuestionPageRenderUnanswered
           subjectName={response.props.subjectName}
           response={response.props.response}
         />
       );
-      const newButton = await screen.findByTestId("question-button-unanswered");
-      await act(async () => {
-      fireEvent.click(newButton)
-      });
+
       const responded = screen.getAllByTestId("question-label-answers")
-      console.log(responded)
       expect(responded[0].textContent).toBe("0 people responded")
       expect(responded[1].textContent).toBe("0 people responded")
       expect(responded[2].textContent).toBe("0 people responded")
-      expect(responded[3].textContent).toBe("0 people responded")
     });
-    it("Answered button returns Boil Water & 2 people responded", async () => {
+    it("Answered button returns answered question ", async () => {
       const params = { subject: "Cooking" };
   
-      var response = await QuestionPage.getServerSideProps({ params });
+      var response = await QuestionPageAnswered.getServerSideProps({ params });
       render(
-        <QuestionPageRender
+        <QuestionPageRenderAnswered
           subjectName={response.props.subjectName}
           response={response.props.response}
         />
       );
-      const newButton = screen.getByTestId("question-button-answered");
-      await act(async () => {
-      fireEvent.click(newButton)
-      });
+  
       const responded = screen.getAllByTestId("question-label-answers")[0]
       const respondedTitle = screen.getAllByTestId("question-label-title")[0]
-      console.log(responded)
       expect(responded.textContent).toBe("2 people responded")
-      expect(respondedTitle.textContent).toBe("Boil Water")
+      expect(respondedTitle.textContent).toBe(" Boil water ")
     });
-  });
+   });
   
